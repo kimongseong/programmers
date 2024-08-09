638e2f75b33dc2aebc7071c22df338aaa788f141
normalhong <rlaghdtjd123@gmail.com>
2024-08-09 15:02:14 +0900

    rink :https://school.programmers.co.kr/learn/courses/30/lessons/12947
    하샤드 수

--- src/Solution.java
+++ src/Solution.java
@@ -5,7 +5,7 @@
 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
 */
 
-class 하샤드수 {
+class HarshadNumber {
     public boolean solution(int x) {
         String xStr = Integer.toString(x);
         int sum = 0;
