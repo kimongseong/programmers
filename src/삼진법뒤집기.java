221d74b6f1399b4f63a756c3398d5c33f6148571
hongseong <ghdtjd1502@gmail.com>
2024-08-14 21:09:29 +0900

    14일 알고리즘 문제풀기

---
+++ src/삼진법뒤집기.java
@@ -0,0 +1,21 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/68935
+문제 설명
+자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+
+제한사항
+n은 1 이상 100,000,000 이하인 자연수입니다.
+ */
+public class 삼진법뒤집기 {
+        public int solution(int n) {
+            int reversed = 0;
+
+
+            while (n > 0) {
+                reversed = reversed * 3 + (n % 3);
+                n /= 3;
+            }
+
+            return reversed;
+        }
+}
