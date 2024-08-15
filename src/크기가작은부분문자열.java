6fadb2738be9b4d79e619b50700af2a45c2583d5
hongseong <ghdtjd1502@gmail.com>
2024-08-15 21:44:52 +0900

    크기가작은부분문자열 for문 ver

--- src/크기가작은부분문자열.java
+++ src/크기가작은부분문자열.java
@@ -24,3 +24,22 @@ public class 크기가작은부분문자열 {
                 .count();
     }
 }
+
+
+//public class Solution {
+//    public int solution(String t, String p) {
+//        long pNum = Long.parseLong(p);
+//        int count = 0;
+//
+//        for (int i = 0; i <= t.length() - p.length(); i++) {
+//            String subStr = t.substring(i, i + p.length());
+//            long subNum = Long.parseLong(subStr);
+//
+//            if (subNum <= pNum) {
+//                count++;
+//            }
+//        }
+//
+//        return count;
+//    }
+//}
